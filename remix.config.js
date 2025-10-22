b024dce38e96dcc0e4486109bd4a2acad6806cc9
Gamingandmusic <4haydenone@gmail.com>
2025-10-21 19:08:36 -0700

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/*.css"],
+  server: "@remix-run/cloudflare-pages",
   serverModuleFormat: "esm",
   future: {
     v3_fetcherPersist: true,
