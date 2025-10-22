e0c9774c268574bac0640d9d9e53273cf502a712
Gamingandmusic <4haydenone@gmail.com>
2025-10-21 19:11:59 -0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,4 +1,4 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+import { createPagesFunctionHandler } from "@remix-run/cloudflare";
 import * as build from "../build/server/index.js";
 
 const handler = createPagesFunctionHandler({ build });
