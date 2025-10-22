e5f7ea939b7ee6a0a52893bf940273b368e5f996
Gamingandmusic <4haydenone@gmail.com>
2025-10-21 18:23:31 -0700

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
   plugins: [
-    tsconfigPaths(),
     remix({
       future: {
         v3_fetcherPersist: true,
