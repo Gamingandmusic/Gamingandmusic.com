b8ef1a90d9773376cd1f16a00a9dc5b6c3f4fbc9
Gamingandmusic <4haydenone@gmail.com>
2025-10-23 18:21:40 -0700

    hugo change

--- functions/[[path]].js
+++
@@ -1,6 +0,0 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import * as build from "../build/server/index.js";
-
-const handler = createPagesFunctionHandler({ build });
-
-export const onRequest = handler;
--- server.js
+++
@@ -1,2 +0,0 @@
-// This file is not used with Cloudflare Pages
-// The functions/[[path]].js handles the request routing
